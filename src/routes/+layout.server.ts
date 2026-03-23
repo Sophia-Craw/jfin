d927672862a91b57f70219fa9d8a8ddbd56b5315
Soph <145169616+Sophia-Craw@users.noreply.github.com>
2026-03-23 14:27:00 -0500

    oops

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ export const load = async ({ params }) => {
     const resp = await fetch(`${JELLYFIN_ADDRESS}/Items?api_key=${API_KEY}`)
     const data = await resp.json()
 
-    const itemResp = await fetch(`https://jellyfin.sink.cat/Items?parentId=${params.id}&recursive=true&api_key=${API_KEY}`)
+    const itemResp = await fetch(`${JELLYFIN_ADDRESS}/Items?parentId=${params.id}&recursive=true&api_key=${API_KEY}`)
     const itemData = await itemResp.json()
 
     return {
