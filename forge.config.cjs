71b8945670b4df1b7124f72a7f976798108410f9
Soph <145169616+Sophia-Craw@users.noreply.github.com>
2026-03-26 16:40:46 -0500

    icon

--- forge.config.cjs
+++ forge.config.cjs
@@ -3,6 +3,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
+    icon: "app.icns",
     asar: {
       unpackDir: "{build,node_modules}"
     },
